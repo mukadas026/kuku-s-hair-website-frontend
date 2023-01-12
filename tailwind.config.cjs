2f7ef463b9e288a550efe00dfc8c054cbd1bfdd4
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-12 18:48:19 +0000

    Adding of new font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				Darker: "#FA58A1",
+				Main: "#FC6DC1",
+				Lighter: "#FE9DE2",
+				Dark: "#010024",
+				Light: "#4D4C70",
+			},
+			fontFamily: {
+				Poppins: "Poppins",
+			},
+		},
 	},
 	plugins: [],
 };
