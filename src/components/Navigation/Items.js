acb3511b479766cd7a5a0ab2cab417b5fed632d8
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-12 18:47:30 +0000

    Starting with the nav component

---
+++ src/components/Navigation/Items.js
@@ -0,0 +1,11 @@
+// This is component contain all the nav  items 
+
+export const navigation = {
+		categories: [
+			{ name: "DISCOVER", href: "#" },
+			{ name: "SHOP", href: "#" },
+			{ name: "CONTACT US", href: "#" },
+			
+		],
+		
+ };
