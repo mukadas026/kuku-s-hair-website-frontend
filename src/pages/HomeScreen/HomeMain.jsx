dabce1026fde6bc6094bebe0293152419fc1fa86
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-01-12 17:54:46 +0000

    Cratiing of all the pages

---
+++ src/pages/HomeScreen/HomeMain.jsx
@@ -0,0 +1,15 @@
+// This component is the main component of the home screen page.  It contains all home components
+import React from 'react'
+import Hero from './Hero'
+
+export const HomeMain = () => {
+  return (
+      <>
+          {/* HOME SCREEN HERO */}
+            <Hero/>
+     </>
+  )
+}
+
+
+export default HomeMain
