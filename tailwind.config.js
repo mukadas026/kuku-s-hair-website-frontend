662f37a12f18142337edcf3fef6b7d3f6b54a01e
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-02-06 22:43:31 +0000

    Adding of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				Darker: "#FA58A1",
+				Main: "#FC6DC1",
+				Lighter: "#FE9DE2",
+				Dark: "#010024",
+				Light: "#4D4C70",
+			},
+			fontFamily: {
+				Poppins: "Poppins",
+			},
+		
+		},
 	},
 	plugins: [],
 };
