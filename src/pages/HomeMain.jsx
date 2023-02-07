f457a4a18cc30c77e8ea45f953a89a08e430683e
Manuel2u <edodoo011@st.ug.edu.gh>
2023-02-07 06:53:29 +0000

    took the hero section to components cus it is not a page its part of homepage so its a component

--- src/pages/HomeMain.jsx
+++ src/pages/HomeMain.jsx
@@ -1,6 +1,6 @@
 // This component is the main component of the home screen page.  It contains all home components
 import React from 'react'
-import Hero from './Hero'
+import Hero from '../components/Hero'
 
 export const HomeMain = () => {
   return (
