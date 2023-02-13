6d08dacf4fb39643106ab36ff95b79603ed33f6c
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-02-13 15:49:47 +0000

    Starting the about us component

---
+++ src/components/AboutUS/About.jsx.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-center py-20'>  Welcome to about Page </h1>
+    </div>
+  )
+}
+
+export default About
+
