262397f8be47c93ea62da8fb3e714a3518b5ecba
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-02-17 12:21:56 +0000

    modified tailwind screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    screens: {
-      sm: "340px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
+  
     extend: {
       colors: {
         Darker: "#FA58A1",
