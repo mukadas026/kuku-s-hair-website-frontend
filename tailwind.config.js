5204ec068523b3b46ec382a040dae76ae41600ab
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-03-05 14:42:17 +0000

    Addding new color for  the about component background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,21 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-  
-    extend: {
-      colors: {
-        Darker: "#FA58A1",
-        Main: "#FC6DC1",
-        Lighter: "#FE9DE2",
-        Dark: "#010024",
-        Light: "#4D4C70",
-      },
-      fontFamily: {
-        Poppins: "Poppins",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				Darker: "#FA58A1",
+				Main: "#FC6DC1",
+				Lighter: "#FE9DE2",
+				Dark: "#010024",
+				Light: "#4D4C70",
+				about_bg: "#F9E8EC",
+			},
+			fontFamily: {
+				Poppins: "Poppins",
+			},
+		},
+	},
+	plugins: [],
 };
