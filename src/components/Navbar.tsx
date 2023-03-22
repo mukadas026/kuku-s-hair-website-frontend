30610bfbbdcc12b6c42cda6bc9a1f33bfc43c63a
Manuel2u <edodoo011@st.ug.edu.gh>
2023-03-22 13:53:26 +0000

    chale the errors bunch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, {  } from "react";
 import { CiSearch, CiUser } from "react-icons/ci";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { MdExpandMore } from "react-icons/md";
+import { Link } from "react-router-dom";
+import SearchModal from "./SearchModal";
 
 const logo = require("../assets/logo.png");
 
@@ -11,13 +13,20 @@ interface NavProp {
 
 // search icon
 
-
 function Navbar({ show }: NavProp) {
+  function setModalOpen(arg0: boolean) {
+    throw new Error("Function not implemented.");
+  }
 
   return (
-
     <>
-      <nav className={show === true ? "flex justify-around mb-1 items-center py-2" : "flex items-center py-4 px-28 border-2 h-[70px]"}>
+      <nav
+        className={
+          show === true
+            ? "flex justify-around mb-1 items-center py-2"
+            : "flex items-center py-4 px-28 border-2 h-[70px]"
+        }
+      >
         <div>
           <img src={logo} className="h-12 w-18 " alt="logo" />
         </div>
@@ -44,10 +53,10 @@ function Navbar({ show }: NavProp) {
               </button>
             </div>
             <div className="flex gap-x-6">
-              <button onClick={() => setModalOpen(true)} >
-              <div className="border-[0.5px] w-8 h-8 p-1 border-primary  rounded-full">
-                <CiSearch size={23} color="#FC6DC1" />
-              </div>
+              <button onClick={() => setModalOpen(true)}>
+                <div className="border-[0.5px] w-8 h-8 p-1 border-primary  rounded-full">
+                  <CiSearch size={23} color="#FC6DC1" />
+                </div>
               </button>
               <div className="pt-1">
                 <AiOutlineShoppingCart size={23} color="#FC6DC1" />
@@ -61,12 +70,10 @@ function Navbar({ show }: NavProp) {
         ) : null}
       </nav>
 
-
       {/*  MODAL  COMPONENT  */}
       <SearchModal
-      
-      modalOpen={modalOpen}
-      ModalCose  ={() => setModalOpen(false)}
+        modalOpen={setModalOpen}
+        ModalCose={() => setModalOpen(false)}
       />
     </>
   );
