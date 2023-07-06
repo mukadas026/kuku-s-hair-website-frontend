0c8615c0249001a3a1477495d86aece37963835c
mukadas026 <mukadasm026@gmail.com>
2023-07-06 03:37:14 +0000

    populate shop and contact page and migrate from CRA to Vite

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,14 +1,77 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import React from "react"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 function Contact() {
-  return (
-    <>
-      <Navbar show={true} />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar show={true} />
+			<div className='w-full max-w-[1980px] bg-red-500 mx-auto'>
+				<div className="w-[75%] mx-auto pt-16">
+					<h3 className="text-3xl font-medium text-primary text-center">Contact Us</h3>
+					<h2 className="text-5xl font-bold text-center">Get in touch</h2>
+					<p className="text-xl text-center">We'd love to hear from you, please fill out this form.</p>
+
+					<form action='' className="mt-16">
+						<div className="flex flex-col md:flex-row justify-around gap-x-20">
+							<label htmlFor='firstName' className="flex flex-col w-full mx-auto">
+								{" "}
+								<span>First Name:</span>
+								<input
+									type='text'
+									name='firstName'
+									id='firstName'
+									placeholder='First Name'
+                  className="w-full block h-12 rounded-lg indent-2 ring-2 ring-gray-600 "
+								/>
+							</label>
+							<label htmlFor='lastName' className="flex flex-col w-full mx-auto">
+								{" "}
+								<span>Last Name:</span>
+								<input
+									type='text'
+									name='lastName'
+									id='lastName'
+									placeholder='Last Name'
+                  className="w-full block h-12 rounded-lg indent-2 ring-2 ring-gray-600 "
+								/>
+							</label>
+						</div>
+
+						<label htmlFor='email' className="flex flex-col w-full mx-auto">
+							{" "}
+							<span>Email:</span>
+							<input
+								type='email'
+								name='email'
+								id='email'
+								placeholder='Email'
+                className="w-full block h-12 rounded-lg indent-2 ring-2 ring-gray-600 "
+							/>
+						</label>
+
+						<label htmlFor='phoneNumber' className="flex flex-col w-full mx-auto">
+							{" "}
+							<span>Phone Number:</span>
+							<input
+								type='tel'
+								name='phoneNumber'
+								id='phoneNumber'
+								placeholder='Phone Number'
+                className="w-full block h-12 rounded-lg indent-2 ring-2 ring-gray-600 "
+							/>
+						</label>
+
+						<textarea
+							name='message'
+							placeholder='Leave us a message'
+						></textarea>
+					</form>
+				</div>
+			</div>
+			<Footer />
+		</>
+	)
 }
 
-export default Contact;
+export default Contact
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+// @ts-ignore
+import { defineConfig } from "vite"
+// @ts-ignore
+import react from "@vitejs/plugin-react-swc"
+// @ts-ignore
+import viteTsconfigPaths from "vite-tsconfig-paths"
+// @ts-ignore
+import svgrPlugin from "vite-plugin-svgr"
+// @ts-ignore
+import eslint from "vite-plugin-eslint"
+// @ts-ignore
+import { checker } from "vite-plugin-checker"
+
+/**
+ * @param match
+ * Regular expression in string or Regexp type,
+ *  or a match predicate  (this: vite transform context, code: string, id: file name string) => void
+ * @returns transformed code
+ */
+import requireTransform from "vite-plugin-require-transform"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	build: {
+		outDir: "build",
+	},
+	plugins: [
+		react(),
+		viteTsconfigPaths(),
+		svgrPlugin(),
+		eslint(),
+		checker({
+			typescript: true,
+		}),
+		requireTransform({}),
+	],
+})
+
+// check the vite-plugin-require-transform params'type
+export type VitePluginRequireTransformParamsType = {
+	//filter files that should enter the plugin
+	// @ts-ignore
+	fileRegex?: RegExp = /.ts$|.tsx$/
+	//prefix that would plugin into the requireSpecifier
+	// @ts-ignore
+	importPrefix? = "_vite_plugin_require_transform_"
+	//to deal with the requireSpecifier
+	importPathHandler?: Function
+}
