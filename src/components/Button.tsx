9f58a92c0b83cc7411b0508b5b921c872fd3c3df
Manuel2u <edodoo011@st.ug.edu.gh>
2023-08-21 17:49:27 +0000

    its working here

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,79 +1,28 @@
-<<<<<<< HEAD
-import React from "react";
-import { BeatLoader } from "react-spinners";
+import React, { FC } from "react";
+import { IconType } from "react-icons";
 
-export interface ButtonPropType {
-  label: string;
-  loading?: boolean;
-  className?: string;
-  onClick?: any;
-  colspan?: number;
+interface PropType {
+  text: string;
+  width?: "full" | "default" | "";
+  Icon?: IconType;
+  handler: Function;
 }
 
-const Button = ({
-  label,
-  loading,
-  className,
-  onClick,
-  colspan,
-}: ButtonPropType) => {
+const Button: FC<PropType> = (props) => {
+  const { handler, text, width, Icon } = props;
   return (
-    <div className={`px-2 col-span-${colspan || 1}`}>
+    <div>
       <button
-        data-testid={label.toLowerCase().replace(/[^A-Z0-9]+/gi, "_")}
-        onClick={onClick}
-        type={onClick ? "button" : "submit"}
-        className={`${
-          className
-            ? className
-            : "text-white bg-blue-600 hover:bg-blue-700 focus:ring-offset-2 focus:ring-blue-700 flex w-full justify-center py-2 px-4 rounded-md border border-transparent shadow-sm text-sm font-medium focus:outline-none"
-        }  disabled:text-white disabled:bg-gray-200 `}
+        onClick={(e) => handler()}
+        className={`text-white bg-primary px-3 h-10 rounded-lg ${
+          width === "full" ? "w-full" : width === "default" ? "" : "w-" + width
+        } ${Icon != undefined ? "flex items-center " : ""}`}
       >
-        {loading ? (
-          <>
-            <div className={` flex justify-center items-center text-blue-800`}>
-              <BeatLoader size={6} color={"#fff"} />
-            </div>
-          </>
-        ) : (
-          <>
-            {/* <span className="self-center">{icon && icon}</span> */}
-            <span className="self-center">{label}</span>
-          </>
-        )}
+        {Icon && <Icon size={20} />}
+        <span className="mx-auto">{text}</span>
       </button>
     </div>
   );
 };
 
 export default Button;
-=======
-import React, { FC } from "react"
-import { IconType } from "react-icons"
-
-interface PropType {
-	text: string
-	width?: "full" | "default" | ""
-	Icon?: IconType
-	handler: Function
-}
-
-const Button: FC<PropType> = (props) => {
-	const { handler, text, width, Icon } = props
-	return (
-		<div>
-			<button
-				onClick={(e) => handler()}
-				className={`text-white bg-primary px-3 h-10 rounded-lg ${
-					width === "full" ? "w-full" : width === "default" ? "" : "w-" + width
-				} ${Icon != undefined ? "flex items-center " : ""}`}
-			>
-				{Icon && <Icon size={20} />}
-				<span className='mx-auto'>{text}</span>
-			</button>
-		</div>
-	)
-}
-
-export default Button
->>>>>>> fadb1e147aaec249def0c15157ee8ec0b66c428a
--- src/components/TRProducts.tsx
+++ src/components/TRProducts.tsx
@@ -9,9 +9,7 @@ function TRProducts() {
         <div className="bg-tertiary w-[85%] py-4 text-2xl flex justify-center mx-auto items-center">
           <h3 className="text-center font-bold ">Top Rated Products</h3>
         </div>
-        <div className="flex gap-x-6 justify-center items-center mt-12 flex-wrap">
-          <Card2 image={wig6} />
-          <Card2 image={wig6} />
+        <div className="flex gap-x-6 gap-y-8 justify-center items-center mt-12 flex-wrap">
           <Card2 image={wig6} />
           <Card2 image={wig6} />
           <Card2 image={wig6} />
